4b9a43135c1243fec5b05afaac75ccf59564c51d
adelaykay <adelekeolasope@gmail.com>
2022-09-08 11:47:04 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(){
+/* A program that prints to the screen */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
