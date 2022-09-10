d3c5d58228e29528069f44d7f84803a74430c155
adelaykay <adelekeolasope@gmail.com>
2022-09-10 05:42:02 +0100

    adv

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = a; b < 58; b++)
+		{
+			for (c = b; c < 58; c++)
+			{
+				if ((a != b) && (a != c) && (b != c))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (!((a == 55) && (b == 56) && (c == 57)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
