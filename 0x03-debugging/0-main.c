676be1e57d76b38417008178bad5b0e1eb8beb65
adelaykay <adelekeolasope@gmail.com>
2022-09-15 20:42:44 +0100

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if an integer is positive or negative
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
