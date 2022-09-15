ccc7c9b161fcf0bedd97721340238547e982bd2c
adelaykay <adelekeolasope@gmail.com>
2022-09-15 17:25:57 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - returns the result of adding two integers
+ *
+ * Return:0  Always (SUCCESS)
+ */
+
+int add(int i, int j)
+{
+		int sum;
+
+		sum = i + j;
+		return sum;
+}
+
