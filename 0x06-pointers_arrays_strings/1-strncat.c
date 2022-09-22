519f8750288dd3d8bcf40242c1bec23350dc558f
adelaykay <adelekeolasope@gmail.com>
2022-09-22 06:39:18 +0100

    task 1 - more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: main string which receives the other string's content
+ * @src: other string
+ * @n: number of bytes from src to use
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			int j = 0;
+
+			while (src[j] != '\0' && j <= n)
+			{
+				dest[i++] = src[j++];
+			}
+			break;
+		}
+	}
+	return (dest);
+}
