e654584b1919740e1da76fd25e15754e955b28f6
adelaykay <adelekeolasope@gmail.com>
2022-10-06 02:10:12 +0100

    corrections

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _isdigit - checks for a digit
+ * @c: integer to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
  * main - adds positive numbers and prints the result
  * @argc: number of arguments
  * @argv: array of arguments
@@ -11,14 +30,21 @@
 
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j = 0, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] < 48 || argv[i][0] > 57)
+		while (argv[i][j])
 		{
-			printf("Error\n");
-			return (1);
+			if (_isdigit(argv[i][j]))
+			{
+				j++;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
