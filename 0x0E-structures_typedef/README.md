dade0c7728fba03502544167512484c96486b4ea
adelaykay <adelekeolasope@gmail.com>
2022-10-19 15:18:02 +0100

    structures task 1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+## 0x0E. C - Structures, typedef
+
+# Tasks
+
+# Resource Links
+
+# My thoughts
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - yeah, a dog struct
+ * @name: first member dog name of type pointer to char
+ * @age: second member dog age of type float
+ * @owner: third member dog owner of type pointer to char
+ *
+ * Description: I'm guessing this is a dog archive
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
