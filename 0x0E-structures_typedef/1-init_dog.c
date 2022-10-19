311a03fffe722049fcafcdff0fa2667f58cacd03
adelaykay <adelekeolasope@gmail.com>
2022-10-19 16:41:31 +0100

    corrections

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -14,7 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -25,4 +25,5 @@ void print_dog(struct dog *d)
 		else
 			printf("Owner: (nil)\n");
 	}
+	return;
 }
