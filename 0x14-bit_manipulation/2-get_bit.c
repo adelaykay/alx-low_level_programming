5a931fec92c8402479d098fa3dea1b3e9ee4925c
adelaykay <adelekeolasope@gmail.com>
2022-10-28 05:04:30 +0100

    get bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,16 +5,16 @@
  * @n: integer number to run check on
  * @index: index, starting from 0 of the bit to get
  *
- * Returns: the value of the bit at index or -1 if an error occurs
+ * Return: the value of the bit at index or -1 if an error occurs
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
-	
+
 	if (index > 63)
 		return (-1);
-	
+
 	mask = 1 << index;
 	return ((n & mask) > 0);
 }
