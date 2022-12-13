afad2f13144609dde6a3aedf350649a14b7516f2
adelaykay <adelekeolasope@gmail.com>
2022-12-13 02:06:42 +0100

    100 operations

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,5 +23,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int i, int j);
+int sub(int i, int j);
+int mul(int i, int j);
+int div(int i, int j);
+int mod(int i, int j);
 
 #endif /** MAIN_H */
